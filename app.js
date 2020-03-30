c9391903c56e62fdb5908f5cb9ec4a293450631c
w0xter <w0xter@github.com>
2020-03-30 02:47:14 +0200

    Enablin client_credentials auth

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const mogoose = require('mongoose');
 const express = require('express');
 const cors	= require('cors');
 const PORT = 3333;
-
+const spotify = require('./middlewares/spotifyController')
 const session = require('./routes/session.router.js')
 const test = require('./routes/test.route.js')
+const bodyParser = require("body-parser");
 let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/test",test)
 app.use("/session", session)
 app.use(cors());
